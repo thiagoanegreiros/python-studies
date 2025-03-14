52cc5b18a2f2d5245c7b8e3d87c45d66316c4f91
Thiago Ananias <thiagoananias@gmail.com>
2025-03-14 16:33:45 -0300

    sample

--- tk/1.1-first-sample.py
+++ tk/1.1-first-sample.py
@@ -1,5 +1,15 @@
-import tkinter
+import tkinter as tk
+from tkinter import messagebox
 
-skylight = tkinter.Tk()
+def Click():
+    replay = messagebox.askquestion("Quit?", "Are you sure?")
+    if replay == 'yes':
+        skylight.destroy();
+
+skylight = tk.Tk()
 skylight.title("Skylight")
+
+button = tk.Button(skylight, text="Bye!", command=Click)
+button.place(x=10, y=10)
+
 skylight.mainloop()
